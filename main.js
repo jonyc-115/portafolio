de49a69fd66ad8f9e1020249e76ae538594b186f
Jhonny <jonycubi_115@hotmail.com>
2023-11-03 11:52:30 -0500

    creando funciones del formulario

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const d = document,
   $menuBtn = d.querySelector(".menu-btn"),
   $menuNav = d.querySelector(".menu-mobile_nav"),
   $iconsMenu = d.querySelectorAll(".menu-mobile_nav a"),
-  $mobile = d.querySelector(".menu-mobile");
+  $mobile = d.querySelector(".menu-mobile"),
+  $header = d.querySelector(".header"),
+  $sections = d.querySelectorAll("section"),
+  $navLinks = d.querySelectorAll(".desk a"),
+  $form = d.querySelector(".form"),
+  $inputName = d.querySelector("#name"),
+  $inputEmail = d.querySelector("#email"),
+  $inputMessage = d.querySelector("#message"),
+  $span = d.createElement("span");
 
 window.addEventListener("scroll", () => {
   let scrollY = window.scrollY;
@@ -14,6 +22,27 @@ window.addEventListener("scroll", () => {
   } else {
     $mobile.style.display = "block";
   }
+
+  if (scrollY > 80) {
+    $header.classList.add("sticky");
+  } else {
+    $header.classList.remove("sticky");
+  }
+
+  // scroll sections
+  $sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      $navLinks.forEach((link) => {
+        link.classList.remove("active");
+        d.querySelector(`.desk a[href*="${id}"]`).classList.add("active");
+      });
+    }
+  });
 });
 
 // funcionamiento del menú versión movil
@@ -34,4 +63,9 @@ $iconsMenu.forEach(($icon) => {
     $menuBtn.innerHTML = '<i class="fa-solid fa-bars"></i>';
     $menuBtn.style.backgroundColor = "#f15906";
   });
+});
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Submit del formulario");
 });
